ac191b0e6217364ebc736f1fdeb3df65e1d47ada
Felipe Canales <felipe.canales.213@gmail.com>
2019-09-28 18:18:22 -0300

    Refactor of duplicated code

--- src/algoritmo/RAM.java
+++ src/algoritmo/RAM.java
@@ -28,30 +28,16 @@ public class RAM implements Algoritmo {
     public int resolver(File palabra1, int largo1, File palabra2, int largo2) {
         byte[] chars1 = getBytes(palabra1);
         byte[] chars2 = getBytes(palabra2);
-        int w = largo2 + 1;
-        int h = largo1 + 1;
-        res = new short[2][w];
-        // 1era fila
-        for (int i = 1; i < w; i++) {
-            res[0][i] = (short)i;
+        short[] izq = new short[largo1 + 1];
+        short[] arr = new short[largo2];
+        for (int i = 1; i < largo1 + 1; i++) {
+            izq[i] = (short)i;
         }
-        for (int j = 1; j < h; j++) {
-            // 1era columna
-            res[1][0] = (short)j;
-            // Otras celdas
-            for (int i = 1; i < w; i++) {
-                int izq = res[1][i-1] + 1;
-                int arr = res[0][i] + 1;
-                int diag = res[0][i-1] + (chars1[j-1] == chars2[i-1]? 0 : 1);
-                int min = izq < arr? izq : arr;
-                res[1][i] = (short)(min < diag? min : diag);
-            }
-            // Cambio de filas
-            res[0] = res[1];
-            res[1] = new short[w];
+        for (int i = 0; i < largo2; i++) {
+            arr[i] = (short)(i + 1);
         }
-
-        return res[0][largo2];
+        resolverSubtabla(arr, izq, chars1, largo1, chars2, largo2);
+        return aInt(res[0][largo2 - 1]);
     }
 
     public short[] resolverSubtabla(short[] bordeSup, short[] bordeIzq,
@@ -61,12 +47,12 @@ public class RAM implements Algoritmo {
         res[0] = bordeSup;
         res[1] = new short[largo2];
         for (int j = 0; j < largo1; j++) {
-            int diag = bordeIzq[j];
-            int izq = bordeIzq[j + 1] + 1;
+            int diag = aInt(bordeIzq[j]);
+            int izq = aInt(bordeIzq[j + 1]) + 1;
             int arr, min;
             for (int i = 0; i < largo2; i++) {
                 diag += (substr1[j] == substr2[i]? 0 : 1);
-                arr = res[0][i] + 1;
+                arr = aInt(res[0][i]) + 1;
                 min = izq < arr? izq : arr;
                 min = min < diag? min : diag;
                 res[1][i] = (short)min;
@@ -82,6 +68,14 @@ public class RAM implements Algoritmo {
         }
         bordeIzq[largo1] = temp;
         return res[0];
+    }
+
+    private int aInt(short x) {
+        int r = x;
+        if (r < 0) {
+            r += 65536;
+        }
+        return r;
     }
 
 }
